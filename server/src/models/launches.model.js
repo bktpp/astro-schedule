add1f06a4cec459bffacbd5ee69a6af710c1b7e0
bktpp <117415722+bktpp@users.noreply.github.com>
2023-12-08 23:43:43 -0600

    connected new launch functionality to frontend

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
    mission: "Kepler Exploration X",
    rocket: "Explorer IS1",
    launchDate: new Date("December 27, 2050"),
-   destination: "Kepler-442 b",
+   target: "Kepler-442 b",
    customer: ["NASA", "Bktp"],
    upcoming: true,
    success: true,
