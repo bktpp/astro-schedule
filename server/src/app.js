8e370e8ca244fc75dd542ec9a3208d358d7cb2b3
bktpp <117415722+bktpp@users.noreply.github.com>
2023-12-08 21:38:49 -0600

    implemented adding new launches in controller and router

--- server/src/app.js
+++ server/src/app.js
@@ -19,8 +19,8 @@ app.use(morgan("combined"));
 app.use(express.json()); //this will parse incoming json from body of request
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use("/planets", planetsRouter);
+app.use("/launches", launchesRouter);
 
 app.get("/*", (req, res) => {
    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
